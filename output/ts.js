6cda75587e1fa8efa060e23712489748e8a42c9f
paravoke <hjohn6863@gmail.com>
2023-06-21 14:49:14 +0100

    working dom

--- output/ts.js
+++ output/ts.js
@@ -1,17 +1,12 @@
 "use strict";
-// Dom select a link from index.html
-let anchor = document.querySelector('a');
-console.log(anchor);
-// trying to set a DOM in the index page 
-// form
-let form = document.querySelector('.new-item-form');
-// inputs
+// get the form from html 
+let form = document.querySelector(".new-item-form");
+// inputs 
 let type = document.querySelector("#type");
 let tofrom = document.querySelector("#tofrom");
 let details = document.querySelector("#details");
 let amount = document.querySelector("#amount");
-// event listener
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.value);
+    console.log("to from => " + tofrom.value + " details " + details.value + " the amount =>");
 });
--- src/ts.ts
+++ src/ts.ts
@@ -1,23 +1,16 @@
-"use strict";
-// Dom select a link from index.html
-let anchor = document.querySelector('a');
-console.log(anchor);
-// trying to set a DOM in the index page 
+"use strict"
+// get the form from html 
+    let form =document.querySelector(".new-item-form") as HTMLFormElement;
 
-// form
-let form = document.querySelector('.new-item-form') as HTMLFormElement;
-
-// inputs
+// inputs 
 let type = document.querySelector("#type") as HTMLSelectElement;
 let tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 let details = document.querySelector("#details") as HTMLInputElement;
-let amount = document.querySelector("#amount") as HTMLInputElement;
+let amount =document.querySelector("#amount") as HTMLInputElement;
 
-// event listener
-form.addEventListener('submit',(e:Event)=>{
+form.addEventListener('submit', (e:Event)=>{
     e.preventDefault();
-    console.log(type.value,
-        tofrom.value,
-        details.value,
-        amount.value,);
+    console.log("to from => "+tofrom.value +" details "+details.value+ " the amount =>");
+
 })
+
